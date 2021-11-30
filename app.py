5f32a07cef2d879f605942378d6d9125bf18ca3d
Will Hunnius <whunnius@uiowa.edu>
2021-11-30 03:30:54 -0600

    init app with procfile

--- app.py
+++ app.py
@@ -12,21 +12,25 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.now)
+    published = db.Column(db.Boolean,default=False)
+
+    
     
     def __repr__(self):
         return '<Task %r>' % self.id
 
 
+#task_content is the entered in poll name by the admin i.e "wewe"
+#tasks is a list of tasks
+
 
 
 @app.route('/', methods=['POST', 'GET'])
-
-
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
+        
         new_task = Todo(content=task_content)
-
         try:
             db.session.add(new_task)
             db.session.commit()
@@ -36,6 +40,7 @@ def index():
 
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
+    
         return render_template('index.html',tasks=tasks)
 
 
@@ -55,6 +60,7 @@ def update(id):
     task = Todo.query.get_or_404(id)
     if request.method == 'POST':
         task.content = request.form['content']
+        print(task.content)
         try:
             db.session.commit()
             return redirect('/')
@@ -62,6 +68,25 @@ def update(id):
             return 'Issue updating Schedule'
     else:
         return render_template('update.html',task=task)
+
+@app.route('/publish/<int:id>')
+def publish(id):
+    task = Todo.query.get_or_404(id)
+    if(task.published==False):
+        task.published =True
+    else:
+        task.published =False
+    try:
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Issue publishing Schedule'
+
+        
+
+
+
+
 if __name__ == "__main__":
     db.create_all()
     app.run(debug=True)
