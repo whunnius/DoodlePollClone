ce68f3bb48f3f3187dbff828d8a02bb92bd5c1ab
Will Hunnius <whunnius@uiowa.edu>
2021-12-03 14:14:43 -0600

    wewew

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from logging import exception
 import re
+import datetime
+import calendar
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timezone
+from datetime import datetime, time, timedelta, date
 from flask_login import UserMixin, login_manager, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from flask_wtf.form import FlaskForm
@@ -25,6 +27,10 @@ login_manager.login_view = "login"
 def load_user(userID):
     return User.query.get(int(userID))
 
+events = [{
+    'todo' : 'Hi',
+    'date' : '2021-11-23',
+}]
 
 
 
@@ -36,6 +42,10 @@ class Todo(db.Model):
     timezone = db.Column(db.String(200), nullable=False)
     location = db.Column(db.String(200), default="")
     description = db.Column(db.String(1000), default="")
+    eventTitle = db.Column(db.String(200), default="")
+    eventDate = db.Column(db.String(200), default="")
+    max_votes = db.Column(db.Integer,default = 0)
+
     
     def __repr__(self):
         return '<Task %r>' % self.id
@@ -74,14 +84,50 @@ class VoterForm(FlaskForm):
     email = StringField(validators=[InputRequired(), Length(min=4,max=40)], render_kw={"placeholder": "Email"})
     notes = StringField(validators=[InputRequired(), Length(min=4,max=10000)], render_kw={"placeholder": "Notes"})
 
+def findDay(date):
+    born = datetime.strptime(date, '%Y %m %d').weekday()
+    return (calendar.day_name[born])
+
+def datetime_range(start, end, delta):
+    current = start
+    while current < end:
+        yield current
+        current += delta
+
 @app.route('/', methods = ['GET'])
 def home():
     return render_template('home.html')
-'''
+@app.route('/voter', methods = ['GET'])
 def voter():
     voter = Todo.query.filter_by(id=1).first() #TODO change filter needs to be by admin
-    return render_template('voter.html', form=VoterForm(), poll_title="Zoom Meeting", location=voter.location, timezone=voter.timezone) #get location, timezone, deadline from db
-'''
+    #TODO get time interval from db
+    timeInterval = 30
+    monthDict={1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', 7:'Jul', 8:'Aug', 9:'Sep', 10:'Oct', 11:'Nov', 12:'Dec'}
+    startDetails = "2021-12-02 09:30" #TODO get from db
+    endDetails = "2021-12-02 11:30" #TODO get from db
+    year = int(startDetails[:4])
+    month = int(startDetails[5:7])
+    day = int(startDetails[8:10])
+    startTimeHour = int(startDetails[11:13])
+    startTimeMin = int(startDetails[14:])
+    endTimeHour = int(endDetails[11:13])
+    endTimeMin = int(endDetails[14:])
+    dts = [dt.strftime('%Y-%m-%d %H:%M ') for dt in 
+       datetime_range(datetime(year, month, day, startTimeHour,startTimeMin), datetime(year, month, day, endTimeHour,endTimeMin), 
+       timedelta(minutes=timeInterval))]
+    timesArr = []
+    for x in dts:
+        timesArr.append(x[11:16])
+    timesArr.append(endDetails[11:])     #adds the last time slot ex./ 10:30-11 if 11 is end time
+    numOfButtons = len(timesArr) - 1
+    stringTimes = []
+    for x in range(len(timesArr)-1):
+        timeSlot = timesArr[x] + '-' + timesArr[x+1]
+        stringTimes.append(timeSlot)
+    #TODO in html page loop through and print out a button for each
+    return render_template('voter.html', form=VoterForm(), pollTitle="Zoom Meeting", location=voter.location, timezone=voter.timezone, year=year,
+        month=monthDict[month], day=day, numOfButtons=numOfButtons, dayType = findDay(startDetails[:10].replace('-',' ')), timeList=timesArr, timeSlot=stringTimes) #get location, timezone, deadline from db
+
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -95,7 +141,7 @@ def login():
                     return redirect(url_for('admin'))
                 else:
                     #Temporary
-                    return redirect(url_for('admin'))
+                    return redirect(url_for('voter'))
 
     return render_template('login.html', form=form)
 
@@ -122,16 +168,19 @@ def register():
 def admin():
     print("im here")
     if request.method == 'POST':
-        
+        task_eventTitle = request.form['eventTitle']
+        task_eventDate = request.form['eventDate']
         task_content = request.form['content']
         task_timezone = request.form['timezone']
         task_location = request.form['location']
         task_description = request.form['description']
+        task_maxVotes = request.form['max_votes']
+
         print(task_content)
         print(task_timezone)
-
+        print(task_eventDate)
         if(task_content != "" and task_timezone!= "" ):
-            new_task = Todo(content=task_content,timezone=task_timezone,location=task_location,description=task_description)
+            new_task = Todo(content=task_content,timezone=task_timezone,location=task_location,description=task_description,eventTitle=task_eventTitle,max_votes=task_maxVotes)
             print("hi")
             try:
                 print("hi")
@@ -149,8 +198,11 @@ def admin():
         print("here now")
         tasks = Todo.query.order_by(Todo.date_created).all()
         print(tasks)
-        return render_template('admin.html',tasks=tasks)
+        return render_template('admin.html',tasks=tasks, events=events)
 
+@app.route('/my-link/')
+def my_link():
+  request.form
 
 @app.route('/delete/<int:id>', methods=['GET', 'POST'])
 def delete(id):
@@ -162,6 +214,9 @@ def delete(id):
         return redirect('/admin')
     except:
         return 'There was a problem deleting specific schedule'
+
+    
+
     
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
