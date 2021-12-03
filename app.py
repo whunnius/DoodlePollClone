9bd90b691e0dcd176a8e159686c533280fad8db2
nihaal <gillnihaal@gmail.com>
2021-12-03 00:19:46 -0600

    table for time slots done with checkboxes done. but checkboxes don't do anything at the moment

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from logging import exception
 import re
+import datetime
+import calendar
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timezone
+from datetime import datetime, time, timedelta, date
 from flask_login import UserMixin, login_manager, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from flask_wtf.form import FlaskForm
@@ -74,13 +76,54 @@ class VoterForm(FlaskForm):
     email = StringField(validators=[InputRequired(), Length(min=4,max=40)], render_kw={"placeholder": "Email"})
     notes = StringField(validators=[InputRequired(), Length(min=4,max=10000)], render_kw={"placeholder": "Notes"})
 
+def findDay(date):
+    born = datetime.strptime(date, '%Y %m %d').weekday()
+    return (calendar.day_name[born])
+
+def datetime_range(start, end, delta):
+    current = start
+    while current < end:
+        yield current
+        current += delta
+
 @app.route('/', methods = ['GET'])
 def home():
     return render_template('home.html')
 '''
 def voter():
     voter = Todo.query.filter_by(id=1).first() #TODO change filter needs to be by admin
-    return render_template('voter.html', form=VoterForm(), poll_title="Zoom Meeting", location=voter.location, timezone=voter.timezone) #get location, timezone, deadline from db
+    #TODO get time interval from db
+    timeInterval = 30
+
+    monthDict={1:'Jan', 2:'Feb', 3:'Mar', 4:'Apr', 5:'May', 6:'Jun', 7:'Jul', 8:'Aug', 9:'Sep', 10:'Oct', 11:'Nov', 12:'Dec'}
+    startDetails = "2021-12-02 09:30" #TODO get from db
+    endDetails = "2021-12-02 11:30" #TODO get from db
+    year = int(startDetails[:4])
+    month = int(startDetails[5:7])
+    day = int(startDetails[8:10])
+    startTimeHour = int(startDetails[11:13])
+    startTimeMin = int(startDetails[14:])
+    endTimeHour = int(endDetails[11:13])
+    endTimeMin = int(endDetails[14:])
+
+    dts = [dt.strftime('%Y-%m-%d %H:%M ') for dt in 
+       datetime_range(datetime(year, month, day, startTimeHour,startTimeMin), datetime(year, month, day, endTimeHour,endTimeMin), 
+       timedelta(minutes=timeInterval))]
+
+    timesArr = []
+    for x in dts:
+        timesArr.append(x[11:16])
+    timesArr.append(endDetails[11:])     #adds the last time slot ex./ 10:30-11 if 11 is end time
+    numOfButtons = len(timesArr) - 1
+
+    stringTimes = []
+    for x in range(len(timesArr)-1):
+        timeSlot = timesArr[x] + '-' + timesArr[x+1]
+        stringTimes.append(timeSlot)
+
+    #TODO in html page loop through and print out a button for each
+    return render_template('voter.html', form=VoterForm(), pollTitle="Zoom Meeting", location=voter.location, timezone=voter.timezone, year=year,
+        month=monthDict[month], day=day, numOfButtons=numOfButtons, dayType = findDay(startDetails[:10].replace('-',' ')), timeList=timesArr, timeSlot=stringTimes) #get location, timezone, deadline from db
 '''
 
 @app.route('/login', methods = ['GET', 'POST'])
